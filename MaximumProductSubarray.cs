50ad53eca8d956f12d05a98531d214ef1f68cb8d
Madhan Kumar <madhan.verizon@gmail.com>
2023-12-24 19:42:02 -0800

    MaximumProductSubarray

---
+++ MaximumProductSubarray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CallStackzSolutions
+{
+    class MaximumProductSubarray
+    {
+        public static int MaxProduct(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+            int maxProduct = nums[0];
+            int minProduct = nums[0];
+            int result = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < 0)
+                {
+                    int temp = maxProduct;
+                    maxProduct = minProduct;
+                    minProduct = temp;
+                }
+                maxProduct = Math.Max(nums[i], maxProduct * nums[i]);
+                minProduct = Math.Min(nums[i], minProduct * nums[i]);
+                result = Math.Max(result, maxProduct);
+            }
+            return result;
+        }
+    }
+}
